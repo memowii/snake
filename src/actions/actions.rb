66db16c82a397d7623797bb008f6dda986c42dac
Guillermo García López <memogl25@gmail.com>
2022-05-24 19:40:25 -0500

    Programando el movimiento de nuestro snake

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -3,7 +3,7 @@ module Actions
     next_direction = state.next_direction
     next_position = calc_next_position(state )
     if position_is_valid?(state, next_position)
-      move_snake(state, next_position)
+      move_snake_to(state, next_position)
     else
       end_game(state)
     end
@@ -25,7 +25,19 @@ module Actions
     end
   end
 
-  def position_is_valid?(state, next_position)
+  def position_is_valid?(state, position)
+    is_invalid = (position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.cols || position.col < 0)
+    return false if is_invalid
+    !(state.snake.positions.include? position)
+  end
 
+  def move_snake_to(state, next_position)
+    new_positions = [next_position] + state.snake.positions[0...-1]
+    state.snake.positions = new_positions
+  end
+
+  def end_game(state)
+    state.game_finished = true
+    state
   end
 end
